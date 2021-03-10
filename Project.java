efa21a8031819f6e5bed83e3f7505b9ac1684921
Abills23 <abills23@icloud.com>
2021-03-10 17:59:14 -0500

    add file

---
+++ Project.java
@@ -0,0 +1,8 @@
+package anna;
+
+public class Project {
+    public static void main(String[] args) {
+        System.out.println("This is my test run");
+    }
+}
+ //C:\Users\abill\IdeaProjects\testproject\src\main\java\anna\Project.java
